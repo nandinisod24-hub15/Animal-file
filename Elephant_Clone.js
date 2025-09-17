488ef6e5b74f325fdb8bf616e0c1a1b390cc348f
nandinisod24-hub15 <nandini.vasudevan.sod24@anu.edu.in>
2025-09-17 23:05:34 +0530

    Add files via upload

---
+++ Elephant_Clone.js
@@ -0,0 +1,9 @@
+class Elephant extends Animal{
+  constructor(br,nm,lang){
+    //create a clone
+    super("Elephant",nm,lang="Trumpet");
+  }
+}
+talk();{
+  print("Hi,nyeahh, I'm a" ${this.breed});
+}
